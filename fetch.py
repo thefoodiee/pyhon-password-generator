0095ee89df59cc7cbad9da4bb3b8491cb8fafa83
thefoodiee <67383814+thefoodiee@users.noreply.github.com>
2021-07-17 12:35:10 +0530

    Update fetch.py
    removed comments

--- fetch.py
+++ fetch.py
@@ -2,8 +2,6 @@ from pprint import pprint
 import time
 import pyperclip
 def process_two_lines(line1: str, line2: str) -> dict:
-    # determine which of the 2 variables is the password
-    # and which is the username
     if 'password' in line1.split('-')[0]:
         passwordline = line1
         userline = line2
@@ -11,7 +9,6 @@ def process_two_lines(line1: str, line2: str) -> dict:
         passwordline = line2
         userline = line1
     
-    # the join is in case the password or username contains a "-" character
     password = '-'.join(passwordline.split('-')[1:]).strip('\n') 
     username = '-'.join(userline.split('-')[1:]).strip('\n')
     service = userline.split('username')[0]
@@ -23,12 +20,11 @@ def process_two_lines(line1: str, line2: str) -> dict:
 
 
 def get_login_info(filename: str) -> dict:
-    # read file
     with open(filename) as infile:
         filecontents = infile.readlines()
     
     result = []
-    # go through lines by pair
+    
     for index, line1 in enumerate(filecontents[::2]):
         result.append(process_two_lines(line1, filecontents[(index*2)+1]))
 
